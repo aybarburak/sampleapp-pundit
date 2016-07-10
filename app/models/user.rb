542fd2e40a629eb836117c31f861d49070f05feb
aybarburak <aybarburak@gmail.com>
2016-07-10 12:44:53 +0200

    Add rank to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  before_save :default_values
+
+  RANKS = {
+      0 => 'User',
+      1 => 'Admin'
+  }
+
+  USER    = 0
+  ADMIN   = 1
+
+  def default_values
+    self.rank ||= USER
+  end
 end
